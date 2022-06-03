f77d63d2385911862851c517bb467ba2c9941a2d
Martin Delgado <martindelgado2011@gmail.com>
2022-06-03 13:30:34 -0300

    Add files via upload

--- models/Scores.js
+++ models/Scores.js
@@ -1,10 +1,21 @@
-export class Scores{
-    constructor(scores){
+export class Scores {
+    constructor(scores) {
         this.scores = scores
     }
 
-    addTableScore(){
+    addTableScore() {
         const scores = JSON.parse(localStorage.getItem("score"))
         console.log(scores)
+        let actualPrintedScores = document.getElementById("tableBody")
+        if (scores !== null) {
+            for (let i = 0; i < scores.length; i++) {
+                row = 
+                `<tr>
+                    <td>${scores[i].name}</td>
+                    <td>${scores[i].score}</td>
+                </tr>`
+                actualPrintedScores += row
+            }
+        }
     }
 }
