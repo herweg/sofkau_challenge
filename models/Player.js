b24da2d3e2d3e38349f412f25702dd9355351fef
Martin Delgado <martindelgado2011@gmail.com>
2022-06-03 11:52:02 -0300

    Update Player.js
    Update del saveScore para que guarde una linea por jugada

--- models/Player.js
+++ models/Player.js
@@ -8,16 +8,33 @@ export class Player {
         this.score += newScore
     }
 
-    saveScore() {
+    // saveScore() {
 
-        var storageScore = sessionStorage.getItem("score")
+    //     var storageScore = sessionStorage.getItem("score")
+    //     if (storageScore === null) {
+    //         var arrNewScore = [this.score]
+    //         sessionStorage.setItem("score", JSON.stringify(arrNewScore))
+    //     } else {
+    //         var arrScore = JSON.parse(storageScore)
+    //         arrScore.push(this.score)
+    //         sessionStorage.setItem("score", JSON.stringify(arrScore))
+    //     }
+    // }
+
+    saveScore(score) {
+
+        const storageScore = localStorage.getItem("score")
         if (storageScore === null) {
-            var arrNewScore = [this.score]
-            sessionStorage.setItem("score", JSON.stringify(arrNewScore))
-        } else {
-            var arrScore = JSON.parse(storageScore)
-            arrScore.push(this.score)
-            sessionStorage.setItem("score" ,JSON.stringify(arrScore))
+            const arrNewScore = [];
+            localStorage.setItem('score', JSON.stringify(arrNewScore))
         }
+        let arrSavedScores = JSON.parse(localStorage.getItem('score'));
+
+        const player = {
+            name: this.name,
+            score: score,
+        }
+        arrSavedScores.push(player)
+        localStorage.setItem('score', JSON.stringify(arrSavedScores))
     }
 }
